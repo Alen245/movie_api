6f09716277e274500eab8b390b6ac0e01b0f1005
Alen245 <akiyanovski24@gmail.com>
2023-04-26 18:13:17 -0400

    update cors

--- index.js
+++ index.js
@@ -17,18 +17,19 @@ const uuid = require("uuid");
 
 //Import cors middleware for allowing requests from different domains(make sure before auth)
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:1234', 'http://testsite.com'];
+// let allowedOrigins = ['http://localhost:1234', 'http://localhost:52455'];
 
-app.use(cors({
-  origin: (origin, callback) => {
-    if (!origin) return callback(null, true);
-    if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
-      let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
-      return callback(new Error(message), false);
-    }
-    return callback(null, true);
-  }
-}));
+// app.use(cors({
+//   origin: (origin, callback) => {
+//     if (!origin) return callback(null, true);
+//     if (allowedOrigins.indexOf(origin) === -1) { // If a specific origin isn’t found on the list of allowed origins
+//       let message = 'The CORS policy for this application doesn’t allow access from origin ' + origin;
+//       return callback(new Error(message), false);
+//     }
+//     return callback(null, true);
+//   }
+// }));
+app.use(cors())
 
 // Import and use passport middleware for authentication
 const passport = require('passport');
