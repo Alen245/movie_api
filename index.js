325ca7bd1dd104f28a62dc497af277aec4327a76
Alen245 <akiyanovski24@gmail.com>
2023-05-30 20:05:25 -0400

    link

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ const uuid = require("uuid");
 
 //Import cors middleware for allowing requests from different domains(make sure before auth)
 const cors = require('cors');
-let allowedOrigins = ['http://localhost:8080', 'https://moviepi24.netlify.app', 'http://localhost:1234', 'https://moviepi24.netlify.app'];
+let allowedOrigins = ['http://localhost:8080', 'https://moviepi24.netlify.app', 'http://localhost:1234', 'http://moviepi24.herokuapp.com'];
 
 app.use(cors({
   origin: (origin, callback) => {
